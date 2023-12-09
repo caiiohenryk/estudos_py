8088aafe9ca84930188d570765350e5c50c4dccd
caiiohenryk <crobatmobile@gmail.com>
2023-12-08 21:05:15 -0300

    second str exercise

---
+++ strings/strings2.py
@@ -0,0 +1,16 @@
+# Continuando o estudo com strings
+
+string = "loucura do mundo"
+
+
+print(string.capitalize())
+# Coloca a primeira letra como maiuscula -- out: "Loucura do mundo"
+
+"""
+    Se voce digitar a variavel que contem a string + . (string.) ira aparecer uma lista
+    de todas as funcoes possiveis pra mudar a string de diversas formas.
+    Exemplo:
+"""
+
+print(string.zfill(100))
+# Preenche uma linha pela esquerda com zeros ate chegar ao tamanho inserido (no caso, 100).
